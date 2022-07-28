0c2e5d5cb610229f109ab9486b57c6f3eb0f08ef
Paul Park <paulpark330@hotmail.com>
2022-07-28 01:30:13 -0700

    added filtering UI

--- src/components/Todos/Todos.tsx
+++ src/components/Todos/Todos.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef } from "react";
+import { Fragment, useCallback, useEffect, useRef, useState } from "react";
 
 import TodoItem from "../TodoItem/TodoItem";
 import styles from "./Todos.module.scss";
@@ -6,21 +6,54 @@ import styles from "./Todos.module.scss";
 import NewTodo from "../NewTodo/NewTodo";
 import { useAppSelector } from "../../hooks/hooks";
 
+import logo from "../../assets/focus360_color.svg";
+
+import { ToggleButton, ToggleButtonGroup } from "@mui/material";
+
 const Todos: React.FC = () => {
+  const [alignment, setAlignment] = useState("all");
   const todos = useAppSelector((state) => state.todos);
   const bottomRef = useRef<HTMLDivElement>(null);
 
-  let todoList;
+  const sortedList = [...todos.items].sort(
+    (a, b) => Number(b.completed) - Number(a.completed)
+  );
 
-  if (!todos.items || todos.items.length === 0) {
-    todoList = (
+  let renderList;
+
+  const handleChange = (
+    event: React.MouseEvent<HTMLElement>,
+    newAlignment: string
+  ) => {
+    setAlignment(newAlignment);
+  };
+
+  let todoList;
+  switch (alignment) {
+    case "all":
+      todoList = todos.items;
+      break;
+    case "incomplete":
+      todoList = todos.items.filter((todo) => todo.completed === false);
+      break;
+    case "completed":
+      todoList = todos.items.filter((todo) => todo.completed === true);
+  }
+
+  if (!todoList || todoList.length === 0) {
+    renderList = (
       <div className={styles.todos_empty}>
-        <p>No todo task yet</p>
+        <img className={styles.logo} src={logo} alt="logo" />
       </div>
     );
   } else {
-    todoList = todos.items.map((item) => {
-      return <TodoItem key={item.id} todo={item} />;
+    renderList = todoList.map((item) => {
+      return (
+        <Fragment key={item.id}>
+          <TodoItem todo={item} />
+          <div ref={bottomRef} />
+        </Fragment>
+      );
     });
   }
 
@@ -39,11 +72,18 @@ const Todos: React.FC = () => {
       <div className={styles.todos_container}>
         <div className={styles.todos_header}>
           <h1>Your Todos</h1>
+          <ToggleButtonGroup
+            color="primary"
+            value={alignment}
+            exclusive
+            onChange={handleChange}
+          >
+            <ToggleButton value="all">All</ToggleButton>
+            <ToggleButton value="incomplete">Incomplete</ToggleButton>
+            <ToggleButton value="completed">Completed</ToggleButton>
+          </ToggleButtonGroup>
         </div>
-        <div className={styles.todos_main}>
-          {todoList}
-          <div ref={bottomRef} />
-        </div>
+        <div className={styles.todos_main}>{renderList}</div>
       </div>
       <NewTodo onAddTodo={scrollToBottom} />
     </>
